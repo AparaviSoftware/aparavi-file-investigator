e9ba66c39ff67b9162a11cc7667ac88661184c22
Joshua Phillips <josh.phillips@aparavi.com>
2025-12-01 15:38:46 -0800

    Small changes.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,38 +6,38 @@ import { ApiProvider } from './contexts/ApiContext'
 import Home from './routes/Home'
 
 function App() {
-  return (
-    <Router>
-      <ApiProvider>
-        <div className="app">
-          <header className="app-header">
-            <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
-              <h1>jeffspeaks.ai</h1>
-            </Link>
-            <div className="header-bottom">
-              <p className="subtitle">Using AI to sort through the gross world of Jeffrey Epstein 🤮</p>
-              <div className="header-buttons">
-                <Link to="/about">
-                  <button className="header-button">About</button>
-                </Link>
-                <button
-                  className="header-button"
-                  onClick={() => window.open('#', '_blank')}
-                >
-                  Buy us coffee
-                </button>
-              </div>
-            </div>
-          </header>
+	return (
+		<Router>
+			<ApiProvider>
+				<div className="app">
+					<header className="app-header">
+						<Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+							<h1>File Investigator</h1>
+						</Link>
+						<div className="header-bottom">
+							<p className="subtitle">Using AI to sort through documents of all kind.</p>
+							<div className="header-buttons">
+								<Link to="/about">
+									<button className="header-button">About</button>
+								</Link>
+								<button
+									className="header-button"
+									onClick={() => window.open('#', '_blank')}
+								>
+									Buy us coffee
+								</button>
+							</div>
+						</div>
+					</header>
 
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </div>
-      </ApiProvider>
-    </Router>
-  )
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/about" element={<About />} />
+					</Routes>
+				</div>
+			</ApiProvider>
+		</Router>
+	)
 }
 
 export default App
